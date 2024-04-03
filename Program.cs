9bb49a8fee0501be2aca4723a2cefc1e6a55d6a6
Ana Chávez Velasco <ana.chavez.velasco@avanade.com>
2024-04-04 00:12:35 +0200

    Base

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float precio = 5.35F;
+            float descuento = 0.2F;
+            float total = precio - (precio * descuento);
+
+            Console.WriteLine($"El precio final del producto es: {total}");
+        }
+    }
+}
